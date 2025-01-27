a730004708870a3a1ad0601d14ac69f4a0d31ed1
Pawan-Dev-Pizone <pawandeep.singh@pizoneinfotech.com>
2025-01-27 12:08:47 +0530

    added ab config setting

--- src/app.js
+++ src/app.js
@@ -1,14 +1,27 @@
-import dotnet from "dotenv";
+import dotenv from "dotenv";
 import express from "express"
+import sequelize  from './db/index.js';
 
 
 const app  = express();
+
 // env file configuration
-dotnet.config({
-    path:'./.env.sample'
+dotenv.config({
+    path:'./.env.local'
 })
 
 app.listen(process.env.PORT,()=>{
-    console.log(`Server is listening to port ${process.env.PORT}`)
+    // CONNECTING TO DB and listening to port
+    establishConnection();
 })
 
+async function establishConnection() {
+    try{
+      await sequelize.authenticate();
+      console.log(`Connection Successfully Established with ${process.env.DB_NAME} database`);
+      console.log(`Server is listening to port ${process.env.PORT}`)
+
+    }catch(error){
+      console.log(`Unable to connect to the ${process.env.DB_NAME} database - `,error);
+    }
+}
